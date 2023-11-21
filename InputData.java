dcba54e74132440ebfe2f806da8bcdf94de79969
VazquezAriel <ariel.renato.av@gmail.com>
2023-11-21 17:53:40 -0500

    Validacion de los datos ingresados por consola

--- InputData.java
+++ InputData.java
@@ -22,4 +22,20 @@ class InputData {
             }
         }
     }
+
+    //Debuelve un numero entre 0 y 4 ingresado por consola.
+    public int getNumberBetween1And5(String message ) {
+        while (true) {
+            try {
+                System.out.println(message);
+                int number =  scanner.nextInt();
+                if (number >= 0 && number <= 4) return number;
+                System.out.println("Porfavor ingrese un numero entero entre 0 y 4.");
+
+            } catch (InputMismatchException e) {
+                System.out.println("Porfavor ingrese un numero entero entre 0 y 4.");
+                scanner.next();
+            }
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
         InputData input = new InputData();
 
         int number = input.getNumber("Ingrese un número entero: ");
-        int row = input.getNumber("Ingrese la ubicación en la Matriz 5x5 - Fila: ");
-        int column = input.getNumber("Ingrese la ubicación en la Matriz 5x5 - Columna: ");
+        int row = input.getNumberBetween1And5("Ingrese la ubicación en la Matriz 5x5 - Fila: ");
+        int column = input.getNumberBetween1And5("Ingrese la ubicación en la Matriz 5x5 - Columna: ");
 
         System.out.println("==================================================");
         System.out.println("- Número ingresado: " + number);
